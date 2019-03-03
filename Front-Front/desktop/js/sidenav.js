9e7a96c5af4e849553b209b8011a09a58449567d
LeonathanBraga <leonathanbraga@id.uff.br>
2019-03-03 11:20:32 -0300

    breakponit para resolucao 320[mobile] ajustes de movimentacao de comps

---
+++ Front-Front/desktop/js/sidenav.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("menu-inferior").style.height = "70vh";
+}
+
+function closeNav() {
+    document.getElementById("menu-inferior").style.height = "10vh";
+}
