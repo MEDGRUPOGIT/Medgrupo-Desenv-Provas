cb93e7bb10135f97606d4e840858dd99851f3974
UsuarioTeste <leonathanbraga@id.uff.br>
2019-03-19 23:13:18 -0300

    ajustada responsividade em menu para dispositivos com altura ate 565px

--- Front-Front/Remake/js/gaveta.js
+++ Front-Front/Remake/js/gaveta.js
@@ -1,7 +1,7 @@
 function gaveta() {
-    if ( document.getElementById("menu-inferior").style.height == "10vh") {
-        document.getElementById("menu-inferior").style.height = "87vh";
+    if ( document.getElementById("menu-inferior").style.height == "10%") {
+        document.getElementById("menu-inferior").style.height = "80%";
     } else {
-        document.getElementById("menu-inferior").style.height = "10vh";
+        document.getElementById("menu-inferior").style.height = "10%";
     }
 }
