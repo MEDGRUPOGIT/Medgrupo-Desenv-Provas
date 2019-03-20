f898d99ca07059010215fe42d2c44a0ea0fdce05
UsuarioTeste <leonathanbraga@id.uff.br>
2019-03-19 22:46:39 -0300

    inserido funcao de gaveta.js e transicao em menu de gaveta

---
+++ Front-Front/Remake/js/gaveta.js
@@ -0,0 +1,7 @@
+function gaveta() {
+    if ( document.getElementById("menu-inferior").style.height == "10vh") {
+        document.getElementById("menu-inferior").style.height = "87vh";
+    } else {
+        document.getElementById("menu-inferior").style.height = "10vh";
+    }
+}
