9851a15b26fddf15d1ad256eb60551301cb75f89
UsuarioTeste <leonathanbraga@id.uff.br>
2019-03-25 00:30:34 -0300

    correcao altura menu gaveta reduzido

--- Front-Front/Remake/js/gaveta.js
+++ Front-Front/Remake/js/gaveta.js
@@ -1,7 +1,7 @@
 function gaveta() {
-    if ( document.getElementById("menu-inferior").style.height == "10%") {
+    if ( document.getElementById("menu-inferior").style.height == "65px") {
         document.getElementById("menu-inferior").style.height = "80%";
     } else {
-        document.getElementById("menu-inferior").style.height = "10%";
+        document.getElementById("menu-inferior").style.height = "65px";
     }
 }
