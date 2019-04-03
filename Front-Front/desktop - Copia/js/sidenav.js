024925f78636544aca3a7ec8cebc35a6e8727121
UsuarioTeste <leonathanbraga@id.uff.br>
2019-04-02 23:12:20 -0300

    alteracoes de ajustes para uso do git pages

--- Front-Front/desktop - Copia/js/sidenav.js
+++
@@ -1,7 +0,0 @@
-function gaveta() {
-    if ( document.getElementById("menu-inferior").style.height == "15vh") {
-        document.getElementById("menu-inferior").style.height = "80vh";
-    } else {
-        document.getElementById("menu-inferior").style.height = "15vh";
-    }
-}
