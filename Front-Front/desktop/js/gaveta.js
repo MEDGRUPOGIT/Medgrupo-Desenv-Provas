6694dcfbeeaf480f5615d11e00340f0435f6e3b7
UsuarioTeste <leonathanbraga@id.uff.br>
2019-04-02 22:44:55 -0300

    teste git docs

---
+++ Front-Front/desktop/js/gaveta.js
@@ -0,0 +1,7 @@
+function gaveta() {
+    if ( document.getElementById("menu-inferior").style.height == "65px") {
+        document.getElementById("menu-inferior").style.height = "80%";
+    } else {
+        document.getElementById("menu-inferior").style.height = "65px";
+    }
+}
