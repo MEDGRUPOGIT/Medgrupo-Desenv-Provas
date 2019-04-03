8539ec542539ec24bfcb354c282f95d804b206b7
UsuarioTeste <leonathanbraga@id.uff.br>
2019-04-02 23:05:12 -0300

    alteracoes de projeto para que possa ser utilizado git pages

--- Front-Front/desktop/js/gaveta.js
+++
@@ -1,7 +0,0 @@
-function gaveta() {
-    if ( document.getElementById("menu-inferior").style.height == "65px") {
-        document.getElementById("menu-inferior").style.height = "80%";
-    } else {
-        document.getElementById("menu-inferior").style.height = "65px";
-    }
-}
