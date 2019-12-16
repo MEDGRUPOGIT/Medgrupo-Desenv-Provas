de8c92303d2196f599d28960835f75be18bcf29b
Luiz Fernando <fernandoaff1@gmail.com>
2019-12-16 15:23:53 -0300

    Filter by filters

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -72,11 +72,34 @@ app.controller("main",["$scope", function($scope){
     let self = this;
 
     $scope.simulados = simulados
+    $scope.expertise = "none"
+    $scope.city = "none"
+    $scope.class = "none"
 
     function filterByRegistration(){
-        let registration = $scope.registration
-        $scope.simulados = simulados.filter( (s) => s.registration == registration)
+        let registration = 
+        $scope.simulados = simulados.filter( (s) => {
+            return s.registration == $scope.registration
+        })
     }
 
+    function filterByFilters(){
+        $scope.simulados = simulados.filter((simulado) => {
+            let allowed = true
+
+            if($scope.expertise != "none")
+                allowed = allowed && simulado.expertise == expertises[$scope.expertise]
+            
+            if($scope.city != "none")
+                allowed = allowed && simulado.city == cities[$scope.city]
+            
+            if($scope.class != "none")
+                allowed = allowed && simulado.class == classes[$scope.class]
+
+            return allowed;
+        })
+    }
+
+    $scope.filterByFilters = filterByFilters
     $scope.filterByRegistration = filterByRegistration;
 }])
